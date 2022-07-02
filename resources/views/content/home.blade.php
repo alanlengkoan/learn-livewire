394fdee6b9f46d3f06045a09f3f30e8b734acc12
alanlengkoan <alansaputralengkoan@gmail.com>
2022-07-02 16:25:26 +0800

    update again

---
+++ resources/views/content/home.blade.php
@@ -0,0 +1,14 @@
+<!-- begin:: base -->
+@extends('base_two')
+<!-- end:: base -->
+
+<!-- begin:: content -->
+@section('content')
+<h2>Halo, Selamat Datang di Belajar Livewire</h2>
+@endsection
+<!-- end:: content -->
+
+<!-- begin:: js local -->
+@section('js')
+@endsection
+<!-- end:: js local -->
